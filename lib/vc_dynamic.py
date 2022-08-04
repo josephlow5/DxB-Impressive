54ea43757a09c91906997433167aac1782fe5bd7
jkk <micngjk01@gmail.com>
2022-08-04 23:21:19 +0800

    Complete vc_dynamic.py

--- lib/vc_dynamic.py
+++ lib/vc_dynamic.py
@@ -1,18 +1,21 @@
-from importlib.resources import path
 import data
 
 #JK
 
 async def process_changes(member, after):
-    name = member.display_name + "的小黑屋"
-    #permissions dict : everyone -> cannot modify, can enter
-    new_voice_channel = await after.guild.create_voice_channel(name,category=after.category, bitrate=256,
-                                                               overwrites=permissions)
-    data.add_subchannel(new_voice_channel.id)
-    #move member to this channel
+    name = member.display_name + "的小黑屋"  
+    
+    new_voice_channel = await after.guild.create_voice_channel(name,category=after.category, bitrate=256)
+    await new_voice_channel.edit(sync_permisions=True)
 
+    await member.move_to(new_voice_channel)
 
-def close_subchannel(after):
-    #delete
-    #remove subchannel
-    path
+    if len(new_voice_channel.voice_states.keys()) == 0:     # 以防进入创建频道channel 然后在创建频道前该user就离开
+        await new_voice_channel.delete()
+    else:
+        data.add_subchannel(new_voice_channel.id)
+
+async def close_subchannel(after):
+    await after.channel.delete()
+
+    data.delete_subchannel(after.channel.id)
