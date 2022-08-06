9f0dad89a3c406a59f2e4ac448c39dd451d2d5cf
josephlow5 <80668891+josephlow5@users.noreply.github.com>
2022-08-06 18:42:37 +0800

    Update chatbot.py

--- lib/chatbot.py
+++ lib/chatbot.py
@@ -1,8 +1,17 @@
 #PL
+#And Jos Because This is quite hard lol
 #auto evil mean, annoying chatbot
 #example -> 有人知道怎么修理bug吗 -> 怎么不去把你的人生修理好
 #example -> 哈咯 -> 哈咯,废物
 #example -> 我失恋了 -> 很好,继续寻找你的下一个失败吧
+
+from neuralintents import GenericAssistant
+from random import randint
+import time
+
+chatbot = GenericAssistant('chatbot.json')
+chatbot.train_model()
+chatbot.save_model()
 
 async def input_chat(message,client):
     pass
