139eaa79adfbc5b7ef919e01188d53149a01b474
josephlow5 <johnlow8782@gmail.com>
2022-08-26 14:25:57 +0800

    New Slash Commands and Dynamic Activities
    Moved all slash commands code into s_command.py.
    Implemented get_teach and oblivate commands.
    
    Implemented dynamic activities with tasks loop of 120 seconds.

--- lib/s_command.py
+++ lib/s_command.py
@@ -3,19 +3,54 @@
 #Create Monitor Channel -> add to data
 #Reply slash command
 
+import json
 from . import data
 from opencc import OpenCC
+import discord
+from discord import app_commands
 
-async def setup(guild,client):
+with open("data/config.json", encoding="utf-8") as configFile:
+    config = json.load(configFile)
+developers = config["developers"]
+
+    
+def slash_command_init(client):
+    @client.tree.command(description="Setup Chatbot and Dynamic Voice Channels in this guild.")
+    async def setup(interaction: discord.Interaction):
+        reply = await s_command_setup(interaction.guild,client)         #3.1 Slash Command - Setup.
+        await interaction.response.send_message(f'{reply}')
+
+    @client.tree.command(description="Teach the bot to response to certain input.")
+    @app_commands.describe(
+        text_receive='What you expect me response to',
+        text_reply='What you expect me response it with',
+    )
+    async def learn(interaction: discord.Interaction, text_receive: str, text_reply: str):
+        reply = await s_command_learn(text_receive,text_reply)          #3.2 Slash Command - Learn.
+        await interaction.response.send_message(f'{reply}', ephemeral=True)
+
+    @client.tree.command(description="Get the current teach txt file of the bot.")
+    async def get_teach(interaction: discord.Interaction):
+        teach = await s_command_get_teach()
+        await interaction.response.send_message(
+            file=teach,ephemeral=True)                                  #3.3 Slash Command - GetTeach.
+
+    @client.tree.command(description="Make the bot forget everything we told to him (Developers Only).")
+    async def obliviate(interaction: discord.Interaction):
+        reply = await s_command_obliviate(interaction.user,client)      #3.4 Slash Command - Setup.
+        await interaction.response.send_message(f'{reply}')
+
+
+async def s_command_setup(guild,client):
     DxB_category = await guild.create_category("DxB")
     DxB_chat_channel = await DxB_category.create_text_channel("我是真的AI")
     data.add_chat_channel(DxB_chat_channel.id)
     DxB_main_channel = await DxB_category.create_voice_channel("大家的黑屋",user_limit=1)
     data.add_channel(DxB_main_channel.id)
-    reply = "DragonXtBlue is fking ready!"
+    reply = "Chatbot and Dynamic Voice Channels are ready to use!"
     return reply
 
-async def learn(text_receive,text_reply):
+async def s_command_learn(text_receive,text_reply):
     cc = OpenCC('s2tw')
     text_receive = cc.convert(text_receive)
     text_reply = cc.convert(text_reply)
@@ -24,3 +59,13 @@ async def learn(text_receive,text_reply):
     reply = "I will reply it next time, thank you for teaching me!"
     return reply
 
+async def s_command_get_teach():
+    return discord.File('data/teach.txt')
+
+async def s_command_obliviate(user,client):
+    if user.id in developers:
+        open("data/teach.txt","w").close()
+        reply = "I am sorry, who are you? Who am I?"
+    else:
+        reply = "Sorry, you cannot use this spell."
+    return reply
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ directory = os.getcwd()
 sys.path.insert(0, directory+"\lib")
 import discord
 from discord import app_commands
+from discord.ext import tasks
 
 #0.5 Import all submodules
 from lib import data
@@ -27,7 +28,7 @@ bot_invite = config["invite"]
 class MyClient(discord.Client):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self)
+        self.tree = app_commands.CommandTree(self)       
     async def setup_hook(self):
         for connected_guild in client.guilds:
             #print(connected_guild.id)
@@ -38,10 +39,15 @@ intents = discord.Intents.all()
 client = MyClient(intents=intents)
 
 
-#3. All kind of events and commands.
+#3. Slash Commands. (Moved to s_command.py)
+s_command.slash_command_init(client)
+
+
+#4. All kind of events.
 @client.event
 async def on_ready():
     await client.setup_hook()
+    change_activity.start()
     print('DragonXtBlue Superb Bot is Fking Ready to Kick some ass!')
 
 recent_chatbot_records = {}
@@ -64,32 +70,42 @@ async def on_message(message):
         if last_response > random.randint(1, 2):
             try:
                 async with message.channel.typing():
-                    await chatbot.input_chat(message,client)            #3.1 Chatbot.
+                    await chatbot.input_chat(message,client)            #4.1 Chatbot.
             except Exception as e:
-                await chatbot.input_chat(message,client)            #3.1 Chatbot.
+                await chatbot.input_chat(message,client)            
                 
 @client.event
 async def on_voice_state_update(member, before, after):                
     if after.channel is not None:
         if data.is_monitor_channel(after.channel.id):
-            await vc_dynamic.process_changes(member, after)      #3.2 Dynamic Voice Channels - Join VC.
+            await vc_dynamic.process_changes(member, after)      #4.2 Dynamic Voice Channels - Join VC.
     if before.channel is not None:
         if data.is_monitor_subchannel(before.channel.id): 
             if len(before.channel.voice_states.keys()) == 0:
-                await vc_dynamic.close_subchannel(before)         #3.2 Dynamic Voice Channels - Leave VC.
+                await vc_dynamic.close_subchannel(before)         #4.2 Dynamic Voice Channels - Leave VC.
 
-    await vc_moderation.process_vc_action(member, before,        #3.3 Mute, Deafen, Stream - Action Moderation.
+    await vc_moderation.process_vc_action(member, before,        #4.3 Mute, Deafen, Stream - Action Moderation.
                                         after, client)
-@client.tree.command()
-async def setup(interaction: discord.Interaction):
-    reply = await s_command.setup(interaction.guild,client)      #3.4 Slash Command - Setup.
-    await interaction.response.send_message(f'{reply}')
-
-@client.tree.command()
-async def learn(interaction: discord.Interaction, text_receive: str, text_reply: str):
-    reply = await s_command.learn(text_receive,text_reply)      #3.5 Slash Command - Learn.
-    await interaction.response.send_message(f'{reply}', ephemeral=True)
     
 
-#4. Run the bot.
+#5. Tasks
+@tasks.loop(seconds=120.0)
+async def change_activity():                                    #5.1 Dynamic Activity
+    with open("data/activity.txt",encoding='utf-8') as activitiesFile:
+        activities = activitiesFile.readlines()
+    new_activities = []
+    for activity in activities:
+        if activity.strip("\n") != "":
+            if activity.strip("\n") not in new_activities:
+                new_activities.append(activity.strip("\n"))
+    activities = new_activities
+    try:
+        selected = random.choice(activities)   
+        activity = discord.Game(name=selected)
+        await client.change_presence(status=discord.Status.online, activity=activity)
+    except:
+        pass
+
+
+#6. Run the bot.
 client.run(bot_token)
